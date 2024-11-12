d137c1d5919c92b28b6189c80792ea40f09e768b
Arthur Kramberger <arthur1curi@gmail.com>
2024-11-12 18:19:01 -0300

    Game seed

--- src/main/java/com/arthurcuri/dslist/DslistApplication.java
+++ src/main/java/com/arthurcuri/dslist/DslistApplication.java
@@ -1,4 +1,4 @@
-package com.arthurcuri.dslist;
+	package com.arthurcuri.dslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
